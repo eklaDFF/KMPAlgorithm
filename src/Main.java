a0d38b7e7d478d1f0f38a0b6112132c608cebcb5
eklaDFF <rahulkrsingh160086@gmail.com>
2024-02-01 20:53:31 +0530

    Added method Creating Prefix Table

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void prefixTable(char[] pattern,int[] prefixTable){
+        int i = 1;
+        int j = 0;
+        prefixTable[0] = 0;
+        while(i<pattern.length){
+            if (pattern[i]==pattern[j]){
+                prefixTable[i] = j + 1;
+                i++;
+                j++;
+            } else if (j>0) {
+                j = prefixTable[j-1];
+            }else {
+                prefixTable[i] = 0;
+                i++;
+            }
+        }
+    }
 }
